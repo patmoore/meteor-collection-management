f2b800b70985a2de0a6de4208320d368358bc438
Patrick Moore <patmoore@farreach.es>
2016-06-20 15:21:13 -0700

    tabs

--- examples/simple-todos/imports/api/TaskManager.js
+++ examples/simple-todos/imports/api/TaskManager.js
@@ -32,50 +32,51 @@ export var TaskManagerType = ManagerType.create({
               owner: Meteor.userId(),
               username: Meteor.user().username,
             });
-          },
-          'remove': {
-              method: function(taskId) {
-                check(taskId, String);
-        
-                const task = Tasks.findOne(taskId);
-                if (task.private && task.owner !== Meteor.userId()) {
-                  // If the task is private, make sure only the owner can delete
-                    // it
-                  throw new Meteor.Error('not-authorized');
-                }
-        
-                Tasks.remove(taskId);
-              },
-          },
-          'setChecked': {
-             method: function(taskId, setChecked) {
-                check(taskId, String);
-                check(setChecked, Boolean);
-        
-                const task = Tasks.findOne(taskId);
-                if (task.private && task.owner !== Meteor.userId()) {
-                  // If the task is private, make sure only the owner can check it
-                    // off
-                  throw new Meteor.Error('not-authorized');
-                }
-             
-                Tasks.update(taskId, { $set: { checked: setChecked } });
-             },
-          },
-          'setPrivate': {
-              method: function(taskId, setToPrivate) {
-                check(taskId, String);
-                check(setToPrivate, Boolean);
-             
-                const task = Tasks.findOne(taskId);
-             
-                // Make sure only the task owner can make a task private
-                if (task.owner !== Meteor.userId()) {
-                  throw new Meteor.Error('not-authorized');
-                }
-             
-                Tasks.update(taskId, { $set: { private: setToPrivate } });
-              },
           }
+      },
+      'remove': {
+          method: function(taskId) {
+            check(taskId, String);
+    
+            const task = Tasks.findOne(taskId);
+            if (task.private && task.owner !== Meteor.userId()) {
+              // If the task is private, make sure only the owner can delete
+                // it
+              throw new Meteor.Error('not-authorized');
+            }
+    
+            Tasks.remove(taskId);
+          },
+      },
+      'setChecked': {
+         method: function(taskId, setChecked) {
+            check(taskId, String);
+            check(setChecked, Boolean);
+    
+            const task = Tasks.findOne(taskId);
+            if (task.private && task.owner !== Meteor.userId()) {
+              // If the task is private, make sure only the owner can check it
+                // off
+              throw new Meteor.Error('not-authorized');
+            }
+         
+            Tasks.update(taskId, { $set: { checked: setChecked } });
+         },
+      },
+      'setPrivate': {
+          method: function(taskId, setToPrivate) {
+            check(taskId, String);
+            check(setToPrivate, Boolean);
+         
+            const task = Tasks.findOne(taskId);
+         
+            // Make sure only the task owner can make a task private
+            if (task.owner !== Meteor.userId()) {
+              throw new Meteor.Error('not-authorized');
+            }
+         
+            Tasks.update(taskId, { $set: { private: setToPrivate } });
+          },
+      }
     }]
 });
