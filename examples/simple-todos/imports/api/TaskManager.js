90e3580b2a63491dbeb34c7be1d83933f3ac473d
Patrick Moore <patmoore@farreach.es>
2016-06-20 15:02:34 -0700

    correct the call prefix

--- examples/simple-todos/imports/api/TaskManager.js
+++ examples/simple-todos/imports/api/TaskManager.js
@@ -2,7 +2,7 @@ import { ManagerType } from 'meteor/patmoore:meteor-collection-management';
 import { Tasks } from 'tasks.js';
 export default ToDoManager = null;
 export var ToDoManagerType = ManagerType.create({
-	callPrefix: 'TaskManager',
+	callPrefix: 'tasks',
 	meteorTopicDefinitions: [{
 		'tasks': {
 			cursor: function tasksPublication() {
