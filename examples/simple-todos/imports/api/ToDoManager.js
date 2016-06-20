04ba6db00e633a78a9987551ba4e0ab009d5a26b
Patrick Moore <patmoore@farreach.es>
2016-06-20 13:29:12 -0700

    todomanager.sj

--- examples/simple-todos/imports/api/ToDoManager.js
+++ examples/simple-todos/imports/api/ToDoManager.js
@@ -3,5 +3,19 @@ import { ManagerType } from 'meteor/patmoore:meteor-collection-management';
 export default ToDoManager = null;
 export var ToDoManager = ManagerType.create({
 	callPrefix: 'ToDoManager',
-
+	meteorTopicDefinitions: [{
+		'tasks': {
+			cursor: function tasksPublication() {
+			    return Tasks.find({
+			      $or: [
+			        { private: { $ne: true } },
+			        { owner: this.userId },
+			      ],
+			    });
+			}
+		}
+	}],
+	meteorCallDefinitions: [{
+		
+	}]
 });
