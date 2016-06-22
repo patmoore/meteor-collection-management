f3c08b14986dee0af34db1677d3e7e731332f58e
Patrick Moore <patmoore@farreach.es>
2016-06-22 15:38:15 -0700

    remove unneeded commented out definitions

--- package.js
+++ package.js
@@ -13,10 +13,10 @@ Package.describe({
 });
 
 Package.onUse(function (api) {
-    api.use(ejson, ['client', 'server']);
-    api.use(underscore, ['client', 'server']);
-    api.use(ecmascript, ['client', 'server']);
-    api.use(mongo, ['client', 'server']);
+    api.use(ejson);
+    api.use(underscore);
+    api.use(ecmascript);
+    api.use(mongo);
     api.use(modules);
     api.mainModule("src/server/index.js", "server");
     api.mainModule("src/client/index.js", "client");
@@ -24,19 +24,10 @@ Package.onUse(function (api) {
     api.export('DbObjectType');
     api.export('ManagerType');
     api.export('Enums');
-    api.export('one');
-    api.export('many');
-    api.export('count');
-    api.export('IronRouterExtension');
-
-//    api.addFiles('src/lib/internalutils.js', ['client', 'server']);
-//    api.addFiles('src/lib/underscoreExtensions.js', ['client', 'server']);
-//    api.addFiles('src/lib/enums.js', ['client', 'server']);
-//    api.addFiles('src/lib/dbobject.js', ['client', 'server']);
-//    api.addFiles('src/lib/manager.js', ['client', 'server']);
-//    api.addFiles('src/client/ironRouterExtensions.js', ['client']);
-//    api.addFiles('src/client/manager.js', ['client' ]);
-//    api.addFiles('src/server/manager.js', 'server');
+    api.export('one', "client");
+    api.export('many', "client");
+    api.export('count', "client");
+    api.export('IronRouterExtension', "client");
 });
 
 Package.onTest(function (api) {
